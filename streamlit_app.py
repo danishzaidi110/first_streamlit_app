11cfdf4ea418af50b0530b0e0605cb0662673273
danishzaidi110 <sdhz123@gmail.com>
2022-08-25 16:48:45 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,9 +20,9 @@ streamlit.dataframe(Fruit_to_show)
 streamlit.header('Fruity Vice Fruit advise!')
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
-# write your own comment -what does the next line do? 
+#streamlit.text(fruityvice_response.json())
+# Normalizes the json format of data into a table  
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
+# the dataframe infers the schema of fruityvice_response and loads the data accordingly 
 streamlit.dataframe(fruityvice_normalized)
 
