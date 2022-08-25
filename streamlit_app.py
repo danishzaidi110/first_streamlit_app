60ad78ceb7fe3f18a19e2590fbfae53af6ceb785
danishzaidi110 <sdhz123@gmail.com>
2022-08-25 16:55:59 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ Fruit_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(Fruit_to_show)
 
 streamlit.header('Fruity Vice Fruit advise!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','cherry')
 streamlit.write('The user entered ', fruit_choice)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/", +fruit_choice)
